6e5efc610db4e985a3e58e29382e3cfa325aff4f
kouchao <admin@jskou.com>
2018-01-02 13:42:51 +0800

    新增 上传图片

--- src/components/food-add.vue
+++ src/components/food-add.vue
@@ -18,14 +18,30 @@
       <el-form-item label="描述">
         <el-input type="textarea" v-model="form.describe"></el-input>
       </el-form-item>
+      <el-form-item label="图片">
+        <el-upload
+          class="avatar-uploader"
+          action=""
+          :show-file-list="false"
+          :before-upload="beforeAvatarUpload">
+          <img v-if="imageUrl" :src="imageUrl" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+
+      </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="saveFood" :loading="loading">添加</el-button>
+        <el-button type="primary" @click="saveFood($event)" :loading="loading">添加</el-button>
       </el-form-item>
     </el-form>
 
   </div>
 </template>
 <script>
+  const ajaxConfig = {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  }
   export default {
     name: "food-list",
     data() {
@@ -39,7 +55,8 @@
           image: ''
         },
         categoryList: [],
-        loading: false
+        loading: false,
+        imageUrl: ''
       }
     },
     created() {
@@ -49,12 +66,12 @@
       this.getCategory()
     },
     methods: {
-      saveFood() {
-
+      saveFood(event) {
+        event.preventDefault();
         if (this.$route.params.id) {
-          this.isEdit()
+          this.isEdit(event)
         } else {
-          this.isAdd()
+          this.isAdd(event)
         }
       },
       getFood() {
@@ -98,9 +115,16 @@
         this.loading = true
         let url = `${this.$baseUrl}/food`;
 
-        let data = this.form
 
-        this.$ajax.post(url, data).then(function (res) {
+        let formData = new FormData();
+        formData.append('name', this.form.name);
+        formData.append('price', this.form.price);
+        formData.append('old_price', this.form.old_price);
+        formData.append('describe', this.form.describe);
+        formData.append('category_id', this.form.category_id);
+        formData.append('image', this.form.image);
+
+        this.$ajax.post(url, formData, ajaxConfig).then(function (res) {
           if (res.data.code == 0) {
             _this.$alert('添加成功', '提示', {
               confirmButtonText: '确定'
@@ -119,9 +143,16 @@
         this.loading = true
         let url = `${this.$baseUrl}/food`;
 
-        let data = this.form
+        let formData = new FormData();
+        formData.append('name', this.form.name);
+        formData.append('price', this.form.price);
+        formData.append('old_price', this.form.old_price);
+        formData.append('describe', this.form.describe);
+        formData.append('category_id', this.form.category_id);
+        formData.append('image', this.form.image);
 
-        this.$ajax.put(url, data).then(function (res) {
+
+        this.$ajax.put(url, formData, ajaxConfig).then(function (res) {
           if (res.data.code == 0) {
             _this.$alert('修改成功', '提示', {
               confirmButtonText: '确定'
@@ -134,12 +165,51 @@
         }).finally(function () {
           _this.loading = false
         })
+      },
+      beforeAvatarUpload(file) {
+        const isJPG = file.type === 'image/jpeg';
+        const isPNG = file.type === 'image/png';
+        const isLt2M = file.size / 1024 / 1024 < 2;
+
+        if (!isJPG && !isPNG) {
+          this.$message.error('上传头像图片只能是 JPG 或者 PNG 格式!');
+        }
+        if (!isLt2M) {
+          this.$message.error('上传头像图片大小不能超过 2MB!');
+        }
+        // this.imageUrl = URL.createObjectURL(file.raw);
+
+        this.form.image = file;
+        return isJPG && isLt2M;
       }
+
     }
 
   }
 </script>
 
 <style scoped>
-
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import axios from 'axios'
+
 import App from './App'
 import router from './router'
+
+
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'font-awesome/css/font-awesome.css'
 
 Vue.prototype.$ajax = axios
+// Vue.prototype.$qiniu = qiniu
+
 Vue.prototype.$baseUrl = 'http://localhost:20001'
 Vue.use(ElementUI)
 
